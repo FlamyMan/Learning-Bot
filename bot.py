cc1e7a5b29b56b41aa7289879ea7f08b775dc727
FlamyMan <99559351+FlamyMan@users.noreply.github.com>
2024-04-13 21:45:23 +0300

    added jisho.org reader to read translates

--- bot.py
+++ bot.py
@@ -1,9 +1,11 @@
-# Импортируем необходимые классы.
 import logging
-from telegram.ext import Application, MessageHandler, filters
+from telegram.ext import Application, MessageHandler, filters, ConversationHandler, CommandHandler
 from config import BOT_TOKEN
 
-# Запускаем логгирование
+class BotStates():
+    EXAM = "examine"
+
+
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.WARNING
 )
@@ -11,36 +13,38 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-# Определяем функцию-обработчик сообщений.
-# У неё два параметра, updater, принявший сообщение и контекст - дополнительная информация о сообщении.
-async def echo(update, context):
-    # У объекта класса Updater есть поле message,
-    # являющееся объектом сообщения.
-    # У message есть поле text, содержащее текст полученного сообщения,
-    # а также метод reply_text(str),
-    # отсылающий ответ пользователю, от которого получено сообщение.
-    await update.message.reply_text(update.message.text)
+async def start(update, context):
+    await update.message.reply_text("Hello this bot doesn't work. Try Later.")
+
+    return ConversationHandler.END
+
+async def examine(update, context):
+    await update.message.reply_text("Hello this command is currently WIP.")
+    return BotStates.EXAM
+
+
+async def stop(update, context):
+    await update.message.reply_text("Have a good day!")
+    return ConversationHandler.END
 
 
 def main():
-    # Создаём объект Application.
-    # Вместо слова "TOKEN" надо разместить полученный от @BotFather токен
     application = Application.builder().token(BOT_TOKEN).build()
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start), CommandHandler('examine', examine)],
 
-    # Создаём обработчик сообщений типа filters.TEXT
-    # из описанной выше асинхронной функции echo()
-    # После регистрации обработчика в приложении
-    # эта асинхронная функция будет вызываться при получении сообщения
-    # с типом "текст", т. е. текстовых сообщений.
-    text_handler = MessageHandler(filters.TEXT, echo)
+        states={
+            BotStates.EXAM: [MessageHandler(filters.TEXT & ~filters.COMMAND, examine)]
+        },
 
-    # Регистрируем обработчик в приложении.
-    application.add_handler(text_handler)
+        fallbacks=[CommandHandler('stop', stop)]
+    )
+    
 
-    # Запускаем приложение.
+    application.add_handler(conv_handler)
+    
+
     application.run_polling()
 
-
-# Запускаем функцию main() в случае запуска скрипта.
 if __name__ == '__main__':
     main()
---
+++ jishoreader.py
@@ -0,0 +1,85 @@
+import math
+import requests
+import subprocess
+import argparse
+import time
+import logging
+import json
+    
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+
+def parse_word(word) -> dict:
+    try:
+        slug = word["slug"]
+        read = word["japanese"][0]["reading"]
+        definitions = [[sens["english_definitions"], sens["parts_of_speech"]] for sens in word["senses"]]
+        return {
+            "slug": slug,
+            "read": read,
+            "definitions": definitions
+                }
+    except Exception:
+        logging.exception(f"Cannot get word{word}")
+        return ""
+
+def parse_json(json) -> list:
+    return [parse_word(word) for word in json]
+
+def get_jisho_data(question: str, page=1, limit=math.inf) -> list:
+    raw = "https://jisho.org/api/v1/search/words?keyword={quest}&page={page}"
+    question = question.replace('#', '%23')
+    question = question.replace(' ', '%20')
+
+    out = []
+    
+    while True:
+        done = raw.format(quest=question, page=page)
+        logging.info(done)
+        try:
+            data = requests.get(done).json()["data"]
+        except Exception as e:
+            logging.exception(e)
+            break
+        if not data:
+            break
+        out += parse_json(data)
+        if page >= limit:
+            break
+        page += 1
+        time.sleep(3)
+    logging.info(f"ended page {page}")
+    return out
+    
+
+def write_jisho_to_file(question: str, page=1, limit=math.inf, output='out.json', show_exp=False):
+    data = get_jisho_data(question, page=page, limit=limit)
+    stringdata = json.dumps(data, ensure_ascii=False)
+    with open(output, "w", encoding="UTF-8") as f:
+        fullpath = f.name
+        f.write(stringdata)
+        if show_exp:
+            subprocess.Popen(rf'explorer /select,{fullpath}')
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('search_for', type=str, help='Your question to https://jisho.org/')
+    parser.add_argument('--start', default=1, type=int, help='First page(min 1)')
+    parser.add_argument('--limit', default=math.inf, type=int, help='Limit page')
+    parser.add_argument('--output', default='out.json', type=str, help='Output file')
+    parser.add_argument('--showExplorer', default=False, type=bool, help='Show File in Explorer after end')
+    parser.add_argument('--DebugMode', default=False, type=bool, help='debug')
+    args = parser.parse_args()
+    _question: str = args.search_for
+    _page = args.start
+    _limit = args.limit
+    _output = args.output
+    _showExp = args.showExplorer
+    
+    write_jisho_to_file(_question, _page, _limit, _output, _showExp)
+
+if __name__ == '__main__':
+    main()
+    
+
